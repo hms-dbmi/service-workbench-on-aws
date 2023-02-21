eaa750c3c02a49f7491161affade38fd8341f073
Luke Sikina <lucas.sikina@gmail.com>
2023-02-21 13:07:12 -0500

    [4196] Filter studies by AWS region
    - Pull AWS region from yaml during scrape
    - Add region to db
    - Filter results for `list` in study service

--- addons/addon-base-raas/packages/base-raas-services/lib/study/study-service.js
+++ addons/addon-base-raas/packages/base-raas-services/lib/study/study-service.js
@@ -518,7 +518,11 @@ class StudyService extends Service {
           .key('category', category)
           .limit(1000)
           .projection(fields)
-          .query();
+          .query()
+          // filter by aws region. Because the scraper might not run until after
+          // this code is deployed we want this filter to be permissive when it isn't
+          // clear if the study is in the correct region
+          .filter(study => ['us-east-1', 'unknown', undefined].includes(study.region))
         break;
 
       default: {
