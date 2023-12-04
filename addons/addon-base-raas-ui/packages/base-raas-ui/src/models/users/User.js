547254fb3f92da0dfcd2db8997cd5addb615a220
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2023-12-04 17:50:09 -0500

    Update user addition to use more stringent email validation. - Update user list filtering to fix bugs where filter doesn't work. - Add sort icons. - Update default sort. - Add created column and remove username column.

--- addons/addon-base-raas-ui/packages/base-raas-ui/src/models/users/User.js
+++ addons/addon-base-raas-ui/packages/base-raas-ui/src/models/users/User.js
@@ -25,6 +25,7 @@ const User = types
     firstName: types.maybeNull(types.optional(types.string, '')),
     lastName: types.maybeNull(types.optional(types.string, '')),
     isAdmin: types.optional(types.boolean, false),
+    createdAt: '',
     username: '',
     ns: types.maybeNull(types.optional(types.string, '')),
     email: '',
