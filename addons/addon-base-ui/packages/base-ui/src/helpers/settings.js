fbd773a1485fc91439185d39ca7820b07cf8721c
Samantha Piatt <samantha.piatt@childrens.harvard.edu>
2024-01-18 10:19:55 -0500

    Actual revert.

--- addons/addon-base-ui/packages/base-ui/src/helpers/settings.js
+++ addons/addon-base-ui/packages/base-ui/src/helpers/settings.js
@@ -37,7 +37,7 @@ const branding = {
   },
   main: {
     title: process.env.REACT_APP_BRAND_MAIN_TITLE,
-    loginWarning: process.env.REACT_APP_LOGIN_WARNING
+    loginWarning: process.env.REACT_APP_LOGIN_WARNING,
   },
   page: {
     title: process.env.REACT_APP_BRAND_PAGE_TITLE,
